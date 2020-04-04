d7df8db3e0b57ee6a446ead3917330acc0d8b35f
helianthus1370@gmail.com <545264likang>
2020-04-04 18:31:18 +0800

    pushed all

---
+++ STOP.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@ Author: HeliantHuS
+@ Codes are far away from bugs with the animal protecting
+@ Time:  4/4/2020
+@ FileName: 海豚暂停与恢复.py
+"""
+import psutil
+import ctypes, sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    ProcessName = "DolphinQ.exe"
+    PID = -1
+
+    for i in psutil.process_iter():
+        if i.name() == ProcessName:
+            PID = i.pid
+
+    dolphinQ = psutil.Process(PID)
+
+    dolphinQ.suspend()
+else:
+    if sys.version_info[0] == 3:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    else:#in python2.x
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", str(sys.executable), str(__file__), None, 1)
